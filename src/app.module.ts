2691c79852d1f4fedef6766f94ffeb29737773e8
benadjal <adame.benadjal@gmail.com>
2022-03-15 17:00:55 +0100

    REMOVE: Delete user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReportModule } from './report/report.module';
-import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 require('dotenv').config();
 
 @Module({
-  imports: [ReportModule
-    , UsersModule,
-    MongooseModule.forRoot('mongodb+srv://admin:' + process.env.MONGODB_PASSWORD + '@cluster0.adwic.mongodb.net/' +
-      process.env.MONGODB_DB_NAME + '?retryWrites=true&w=majority')],
+  imports: [ReportModule, MongooseModule.forRoot('mongodb+srv://admin:' + process.env.MONGODB_PASSWORD + '@cluster0.adwic.mongodb.net/' +
+    process.env.MONGODB_DB_NAME + '?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
