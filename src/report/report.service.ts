4e20151168a9318313f0b119c7231ff022165686
benadjal <adame.benadjal@gmail.com>
2022-03-18 11:26:05 +0100

    DELETE: User module

--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { InsertResult, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { ReportDto } from './dto/report.dto';
 import { EtatLieux } from './entities/EtatLieux.entity';
 import { Report } from './entities/Report.entity';
@@ -12,14 +12,13 @@ export class ReportService {
     constructor(
         @InjectRepository(Report)
         private reportRepository: Repository<Report>,
-        
+
         @InjectRepository(EtatLieux)
         private etatLieuxRepository: Repository<EtatLieux>
     ) { }
-
-
+    
     async getAllReports(): Promise<Report[]> {
-        let reports = await this.reportRepository.find({relations:["listetatLieux"]})
+        let reports = await this.reportRepository.find({ relations: ["listetatLieux"] })
         console.log(reports);
         return reports;
     }
@@ -31,17 +30,12 @@ export class ReportService {
             report.nameSite,
             report.nameGuardian
         );
-        console.log(reportEnity);
         const addReport = await this.reportRepository.insert(reportEnity);
 
         forEach(report.listetatLieux, async (etat: EtatLieux) => {
             let etatLieuxEnity = new EtatLieux(etat.etatLieux, etat.etatLieuxDesc);
             etatLieuxEnity.report = reportEnity;
-            console.log(etatLieuxEnity);
             const addEtatReport = await this.etatLieuxRepository.insert(etatLieuxEnity)
         })
-
     }
-
-
 }
