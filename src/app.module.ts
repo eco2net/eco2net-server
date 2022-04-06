c769b605d69bebee8982166357119249975ef125
benadjal <adame.benadjal@gmail.com>
2022-04-06 17:01:33 +0200

    ADD: Connect to s3 bucket

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,24 @@ import { AppService } from './app.service';
 import { ReportModule } from './report/report.module';
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { PhotosModule } from './photos/photos.module';
+import { ConfigModule } from '@nestjs/config';
+import databaseConfig from './config/database.config';
+import * as Joi from 'joi';
 
 require('dotenv').config();
 
 @Module({
-  imports: [ReportModule,
+  imports: [
+    ConfigModule.forRoot({
+      load:[databaseConfig],
+      validationSchema: Joi.object({
+        AWS_REGION: Joi.string().required(),
+        AWS_ACCESS_KEY_ID: Joi.string().required(),
+        AWS_SECRET_ACCESS_KEY: Joi.string().required(),
+        AWS_PUBLIC_BUCKET_NAME: Joi.string().required(),
+      })
+    }),
+    ReportModule,
     TypeOrmModule.forRoot({
       name: 'default',
       type: 'postgres',
