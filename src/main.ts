f339b765c7e646da334711f2e2751103249ad4fd
benadjal <adame.benadjal@gmail.com>
2022-09-08 13:00:44 +0200

    FIX : origin prod

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ require('dotenv').config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-      origin: ['http://localhost:8080', 'https://thirsty-swirles-470da8.netlify.app/' ],
+      origin: ['http://localhost:8080', 'https://thirsty-swirles-470da8.netlify.app' ],
       credentials: true,
   });
   
